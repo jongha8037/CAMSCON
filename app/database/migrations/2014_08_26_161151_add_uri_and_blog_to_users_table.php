4465d36fe18c8c319fa6731b58f8eca51a04b700
CafeCoder <coffeeshopcoder@users.noreply.github.com>
2014-08-26 16:18:07 +0900

    Added uri and blog fields to users table via migration.

---
+++ app/database/migrations/2014_08_26_161151_add_uri_and_blog_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUriAndBlogToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table) {
+			$table->string('uri')->nullable()->after('gender');
+			$table->string('blog')->nullable()->after('uri');
+			$table->index('uri');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schemma::table('users' function() {
+			$table->dropColumn(array('uri', 'blog'));
+		});
+	}
+
+}
