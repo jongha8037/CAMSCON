f83aa6ac3f26ff72b690323f8a47a12a2a9e7d72
CafeCoder <coffeeshopcoder@users.noreply.github.com>
2014-08-27 21:09:12 +0900

    Changed path to user profile image storage in ProfileImage model.

--- app/models/ProfileImage.php
+++ app/models/ProfileImage.php
@@ -2,7 +2,7 @@
 
 class ProfileImage extends ImageAttachment {
 
-	protected $relative_path='user/profile-images';
+	protected $relative_path='user-assets/profile-images';
 
 	public function user() {
 		return $this->belongsTo('User');
