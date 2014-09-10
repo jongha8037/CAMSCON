9e3449e83f028b12467cc85857e151bbebeb8cc4
CafeCoder <coffeeshopcoder@users.noreply.github.com>
2014-09-10 16:29:22 +0900

    Added styling for profile view filter tabs.

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -4,14 +4,20 @@ class ProfileController extends BaseController {
 	public function showProfile($id) {
 		if(strval(intval($id))==$id) {
 			//Get profile by id
-			$profile=User::find($id);
+			$profile=User::with('profileImage', 'snaps')->find($id);
 		} else {
 			//Get profile by uri
-			$profile=User::where('uri', '=', $id)->first();
+			$profile=User::with('profileImage', 'snaps')->where('uri', '=', $id)->first();
 		}
 
 		if($profile) {
-			{{$profile}}
+			ViewData::add('profile', $profile);
+			$stats=new stdClass();
+			$stats->posts=$profile->snaps->count();
+			$stats->likes=0;
+			$stats->comments=0;
+			ViewData::add('stats', $stats);
+			return View::make('front.user.profile', ViewData::get());
 		} else {
 			App::abort(404);
 		}
