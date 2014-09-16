13cbc624b20c154dd2a182e8d07e215504fcf386
CafeCoder <coffeeshopcoder@users.noreply.github.com>
2014-09-16 19:48:53 +0900

    Added overriding url resolving method to ImageAttachment childrens.

--- app/models/StreetSnapPrimary.php
+++ app/models/StreetSnapPrimary.php
@@ -9,4 +9,13 @@ class StreetSnapPrimary extends ImageAttachment {
 		return $this->belongsTo('StreetSnap', 'snap_id');
 	}
 
+	//Override method for AWS CloudFront
+	public function getUrlAttribute() {
+		if(isset($this->id,$this->original_extension,$this->dir_path,$this->filename)) {
+			return sprintf('http://d10wt5d2fmm040.cloudfront.net/%s/%s.%s', $this->dir_path, $this->filename, $this->original_extension);
+		} else {
+			return false;
+		}
+	}//getUrlAttribute()
+
 }
