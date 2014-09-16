6362208ce2b694e08857fa39f7e4848bd807ffb6
CafeCoder <coffeeshopcoder@users.noreply.github.com>
2014-09-17 01:53:07 +0900

    Changed cloudfront url to CNAME

--- app/models/ProfileCover.php
+++ app/models/ProfileCover.php
@@ -8,4 +8,13 @@ class ProfileCover extends ImageAttachment {
 		return $this->belongsTo('User');
 	}
 
+	//Override method for AWS CloudFront
+	public function getUrlAttribute() {
+		if(isset($this->id,$this->original_extension,$this->dir_path,$this->filename)) {
+			return sprintf('http://cdn.camscon.kr/%s/%s.%s', $this->dir_path, $this->filename, $this->original_extension);
+		} else {
+			return false;
+		}
+	}//getUrlAttribute()
+
 }
--- app/models/StreetSnapPrimary.php
+++ app/models/StreetSnapPrimary.php
@@ -12,7 +12,7 @@ class StreetSnapPrimary extends ImageAttachment {
 	//Override method for AWS CloudFront
 	public function getUrlAttribute() {
 		if(isset($this->id,$this->original_extension,$this->dir_path,$this->filename)) {
-			return sprintf('http://d10wt5d2fmm040.cloudfront.net/%s/%s.%s', $this->dir_path, $this->filename, $this->original_extension);
+			return sprintf('http://cdn.camscon.kr/%s/%s.%s', $this->dir_path, $this->filename, $this->original_extension);
 		} else {
 			return false;
 		}
