46295b61a81d87336e4533890ce276b8cbf944d4
CafeCoder <coffeeshopcoder@users.noreply.github.com>
2014-10-23 23:03:39 +0900

    Added override link to admin groups view.

--- app/views/admin/users/groups.blade.php
+++ app/views/admin/users/groups.blade.php
@@ -139,7 +139,7 @@ User groups
 		<tbody>
 		@foreach($users as $user)
 			@if($user->id==375 || $user->id==7133)
-			<tr><td><input type="checkbox" name="selected_users[]" value="{{$user->id}}" /></td><td>{{$user->id}}</td><td>{{{$user->nickname}}} <a href=""><span class="glyphicon glyphicon-fire"></span></a></td><td>{{$user->email}}</td><td>{{$user->gender}}</td><td>{{$user->created_at}}</td></tr>
+			<tr><td><input type="checkbox" name="selected_users[]" value="{{$user->id}}" /></td><td>{{$user->id}}</td><td>{{{$user->nickname}}} <a href="{{action('AdminController@overrideUser', $user->id)}}"><span class="glyphicon glyphicon-fire"></span></a></td><td>{{$user->email}}</td><td>{{$user->gender}}</td><td>{{$user->created_at}}</td></tr>
 			@else
 			<tr><td><input type="checkbox" name="selected_users[]" value="{{$user->id}}" /></td><td>{{$user->id}}</td><td>{{{$user->nickname}}}</td><td>{{$user->email}}</td><td>{{$user->gender}}</td><td>{{$user->created_at}}</td></tr>
 			@endif
