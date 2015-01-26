27e85eebb8c2e06ed1c2c68fdf943ed137cc9014
CafeCoder <cafecoder@naver.com>
2015-01-26 20:13:25 +0900

    Started working on the recruiting page for Season 7.

---
+++ app/views/front/recruiting/season-7.blade.php
@@ -0,0 +1,21 @@
+@extends('front.layouts.master')
+
+@section('head_title')
+Camtographer Season 7 Recruiting - CAMSCON
+@stop
+
+@section('head_styles')
+<!--FB Open Graph tags-->
+<meta property="og:title" content="" />
+<meta property="og:site_name" content="CAMSCON" />
+<meta property="og:url" content="{{url('recruiting/season-7')}}" />
+<meta property="og:description" content="" />
+<meta property="og:image" content="http://cdn.camscon.kr/front-assets/layouts/fb_og.jpg" />
+<meta property="fb:app_id" content="562009567255774" />
+<meta property="og:locale" content="ko_KR" />
+
+<link rel="stylesheet" type="text/css" href="{{asset('front-assets/user-feedback/form.css')}}">
+@stop
+
+@section('content')
+@stop
